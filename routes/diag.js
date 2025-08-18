91076e15d367ae85591d5d26d63af2debbebda9c
gonzalo0909 <lapacasa22@gmail.com>
2025-08-18 15:41:57 -0300

    Update diag.js

--- routes/diag.js
+++ routes/diag.js
@@ -1,32 +1,18 @@
-// routes/diag.js
 "use strict";
 
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const envKeys = [
-    "BASE_URL",
-    "BOOKINGS_WEBAPP_URL",
-    "MP_ACCESS_TOKEN",
-    "STRIPE_SK",
-    "STRIPE_WEBHOOK_SECRET",
-    "ADMIN_USER",
-    "ADMIN_PASS",
-    "CRON_TOKEN",
-    "HOLD_TTL_MINUTES",
-  ];
-
-  const env = {};
-  envKeys.forEach((k) => {
-    env[k] = process.env[k] ? true : false;
-  });
-
   res.json({
     ok: true,
-    commit: process.env.COMMIT_HASH || "dev",
     now: new Date().toISOString(),
-    env,
+    commit: process.env.COMMIT_SHA || "dev",
+    env: {
+      STRIPE: !!process.env.STRIPE_SK,
+      MP: !!process.env.MP_ACCESS_TOKEN,
+      SHEETS: !!process.env.BOOKINGS_WEBAPP_URL
+    }
   });
 });
 
