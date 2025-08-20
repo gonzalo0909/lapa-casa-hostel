9bfa534c7f3405f537d94cb46956c667bdee78ea
gonzalo0909 <lapacasa22@gmail.com>
2025-08-19 21:48:55 -0300

    Create config.js

---
+++ public/book/config.js
@@ -0,0 +1,4 @@
+// Config pública (no sensible). Ajustá BACKEND_BASE_URL si sirve bajo /api.
+window.BACKEND_BASE_URL = window.BACKEND_BASE_URL || "/api";
+window.STRIPE_PUBLISHABLE_KEY = window.STRIPE_PUBLISHABLE_KEY ||
+  "pk_test_51Ru1ZgGcpzl4zlUFGhibCAqJMKWCse2m2auKYWQkGxBIQ9ZtoGvPwoyrjwzmZYMEq1mbL4XZEo3TP7cuhhUHgtg000iI4zzsRh";
