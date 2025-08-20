4a598cda8023daef24bec9a79cd3b8f221eb7c3d
Lapa Casa <lapacasa22@gmail.com>
2025-08-20 17:29:10 -0300

    Update events.js

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,9 +1,11 @@
 "use strict";
 const express = require("express");
+const { getEvents } = require("../services/events");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ ok: true, events: [] });
+router.get("/", async (_req, res) => {
+  const events = await getEvents();
+  res.json({ ok: true, events });
 });
 
 module.exports = router;
