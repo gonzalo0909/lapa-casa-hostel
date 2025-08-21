1276ae956ff87081111ad275ecd8dfd6bdef9c82
Lapa Casa <lapacasa22@gmail.com>
2025-08-21 00:19:39 -0300

    Delete src directory

--- src/services/events.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-async function getEvents() {
-  // Demo: reemplaza por fetch a tus FEEDS si querés
-  return [{ title: "Evento demo", date: new Date().toISOString() }];
-}
-
-module.exports = { getEvents };
