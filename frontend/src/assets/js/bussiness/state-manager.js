29679eba7b79eff494cce89d51e2955d9d2f735c
gonzalo0909 <lapacasa22@gmail.com>
2025-09-04 21:50:42 -0300

    Create state-manager.js

---
+++ frontend/src/assets/js/bussiness/state-manager.js
@@ -0,0 +1,119 @@
+class StateManager {
+  constructor() {
+    this.state = {
+      currentStep: 'search',
+      searchCriteria: null,
+      availabilityData: null,
+      selectedBeds: [],
+      formData: {},
+      paymentInfo: null,
+      bookingData: null
+    };
+  }
+  
+  getCurrentStep() {
+    return this.state.currentStep;
+  }
+  
+  updateStep(step) {
+    this.state.currentStep = step;
+    this.saveState();
+  }
+  
+  setSearchCriteria(dateIn, dateOut, men, women) {
+    this.state.searchCriteria = { dateIn, dateOut, men, women };
+    this.saveState();
+  }
+  
+  getSearchCriteria() {
+    return this.state.searchCriteria;
+  }
+  
+  setAvailabilityData(data) {
+    this.state.availabilityData = data;
+    this.saveState();
+  }
+  
+  isAvailabilityDataValid() {
+    return !!this.state.availabilityData;
+  }
+  
+  getAvailabilityData() {
+    return this.state.availabilityData;
+  }
+  
+  setSelectedBeds(beds) {
+    this.state.selectedBeds = beds;
+    this.saveState();
+  }
+  
+  getSelectedBeds() {
+    return this.state.selectedBeds;
+  }
+  
+  clearSelectedBeds() {
+    this.state.selectedBeds = [];
+  }
+  
+  setFormData(field, value) {
+    this.state.formData[field] = value;
+    this.saveState();
+  }
+  
+  getFormData() {
+    return this.state.formData;
+  }
+  
+  setPaymentInfo(info) {
+    this.state.paymentInfo = info;
+    this.saveState();
+  }
+  
+  getPaymentInfo() {
+    return this.state.paymentInfo;
+  }
+  
+  setBookingData(data) {
+    this.state.bookingData = data;
+    this.saveState();
+  }
+  
+  getBookingData() {
+    return this.state.bookingData;
+  }
+  
+  clearHold() {
+    this.state.selectedBeds = [];
+  }
+  
+  getCompleteBookingData() {
+    return { ...this.state };
+  }
+  
+  reset() {
+    this.state = {
+      currentStep: 'search',
+      searchCriteria: null,
+      availabilityData: null,
+      selectedBeds: [],
+      formData: {},
+      paymentInfo: null,
+      bookingData: null
+    };
+    this.clearSavedState();
+  }
+  
+  saveState() {
+    try {
+      localStorage.setItem('hostelBookingState', JSON.stringify(this.state));
+    } catch (e) {
+      console.warn('No se pudo guardar el estado');
+    }
+  }
+  
+  clearSavedState() {
+    localStorage.removeItem('hostelBookingState');
+  }
+}
+
+window.stateManager = new StateManager();
