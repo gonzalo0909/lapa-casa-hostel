3950367a424a18987f558d9f35ff3978d24d310f
Lapa Casa <lapacasa22@gmail.com>
2025-09-06 07:48:32 -0300

    Update state-manager.js

--- frontend/src/assets/js/bussiness/state-manager.js
+++ frontend/src/assets/js/bussiness/state-manager.js
@@ -1,5 +1,6 @@
 class StateManager {
   constructor() {
+    this.memoryStorage = new Map();
     this.state = {
       currentStep: 'search',
       searchCriteria: null,
@@ -10,82 +11,86 @@ class StateManager {
       bookingData: null
     };
   }
-
+  
   getCurrentStep() {
     return this.state.currentStep;
   }
-
+  
   updateStep(step) {
     this.state.currentStep = step;
     this.saveState();
   }
-
+  
   setSearchCriteria(dateIn, dateOut, men, women) {
     this.state.searchCriteria = { dateIn, dateOut, men, women };
     this.saveState();
   }
-
+  
   getSearchCriteria() {
     return this.state.searchCriteria;
   }
-
+  
   setAvailabilityData(data) {
     this.state.availabilityData = data;
     this.saveState();
   }
-
+  
   isAvailabilityDataValid() {
     return !!this.state.availabilityData;
   }
-
+  
+  getAvailabilityData() {
+    return this.state.availabilityData;
+  }
+  
   setSelectedBeds(beds) {
     this.state.selectedBeds = beds;
     this.saveState();
   }
-
+  
   getSelectedBeds() {
     return this.state.selectedBeds;
   }
-
+  
   clearSelectedBeds() {
     this.state.selectedBeds = [];
   }
-
+  
   setFormData(field, value) {
     this.state.formData[field] = value;
     this.saveState();
   }
-
+  
   getFormData() {
     return this.state.formData;
   }
-
+  
   setPaymentInfo(info) {
     this.state.paymentInfo = info;
     this.saveState();
   }
-
+  
   getPaymentInfo() {
     return this.state.paymentInfo;
   }
-
+  
   setBookingData(data) {
     this.state.bookingData = data;
     this.saveState();
   }
-
+  
   getBookingData() {
     return this.state.bookingData;
   }
-
+  
   clearHold() {
     this.state.selectedBeds = [];
   }
-
+  
   getCompleteBookingData() {
     return { ...this.state };
   }
-
+  
   reset() {
     this.state = {
       currentStep: 'search',
@@ -98,17 +103,34 @@ class StateManager {
     };
     this.clearSavedState();
   }
-
+  
   saveState() {
     try {
-      localStorage.setItem('hostelBookingState', JSON.stringify(this.state));
+      this.memoryStorage.set('hostelBookingState', { ...this.state });
     } catch (e) {
       console.warn('No se pudo guardar el estado');
     }
   }
-
+  
+  loadState() {
+    try {
+      const saved = this.memoryStorage.get('hostelBookingState');
+      if (saved) {
+        this.state = { ...saved };
+        return true;
+      }
+    } catch (e) {
+      console.warn('No se pudo cargar el estado');
+    }
+    return false;
+  }
+  
   clearSavedState() {
-    localStorage.removeItem('hostelBookingState');
+    this.memoryStorage.delete('hostelBookingState');
+  }
+  
+  destroy() {
+    this.memoryStorage.clear();
   }
 }
 
