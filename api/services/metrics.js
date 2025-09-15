a927c4eaa9918fdaa5a285a762e90f2988e70c7f
gonzalo0909 <lapacasa22@gmail.com>
2025-09-15 01:07:21 -0300

    Update metrics.js

--- api/services/metrics.js
+++ api/services/metrics.js
@@ -2,7 +2,7 @@
 
 /**
  * services/metrics.js
- * Exposición de métricas Prometheus
+ * Prometheus metrics: default + HTTP duration + request count
  */
 
 const client = require("prom-client");
@@ -12,16 +12,29 @@ client.collectDefaultMetrics({ register: registerMetrics });
 
 const httpRequestDuration = new client.Histogram({
   name: "http_request_duration_seconds",
-  help: "Duración de requests HTTP en segundos",
+  help: "HTTP request duration in seconds",
   labelNames: ["method", "route", "status_code"],
-  buckets: [0.1, 0.3, 1.5, 5, 10]
+  buckets: [0.05, 0.1, 0.3, 1.5, 5, 10]
 });
 registerMetrics.registerMetric(httpRequestDuration);
+
+const httpRequestCount = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "status_code"]
+});
+registerMetrics.registerMetric(httpRequestCount);
 
 function metricsMiddleware(req, res, next) {
   const end = httpRequestDuration.startTimer();
   res.on("finish", () => {
-    end({ method: req.method, route: req.originalUrl, status_code: res.statusCode });
+    const labels = {
+      method: req.method,
+      route: req.route?.path || req.originalUrl || "unknown",
+      status_code: res.statusCode
+    };
+    end(labels);
+    httpRequestCount.inc(labels);
   });
   next();
 }
